edef13538b5656e6b40696bacee626a8f7ba7cc6
vishwanathmx <vmathapati@xpanxion.com>
2019-05-13 17:46:18 +0530

    demo 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo1';
+  title = 'demo1nhiii';
+
+  imgsrc ='../assets/galaxy.jpg';
+  imgsrc2='../assets/mygalaxy.jpg';
+
+  toggle() {
+   if(this.imgsrc=='../assets/galaxy.jpg'){
+    this.imgsrc=this.imgsrc2;
+   }
+   else{
+    this.imgsrc='../assets/galaxy.jpg';
+   }
+
+  }
 }
